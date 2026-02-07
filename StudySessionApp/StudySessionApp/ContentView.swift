884fce5719b4be0a74562836d16e5e071290b026
Max Finch <maxfinch@s172-20-67-h78.paws.uga.edu>
2026-02-06 22:01:12 -0500

    started front end part on xcode

---
+++ StudySessionApp/StudySessionApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudySessionApp
+//
+//  Created by Max Finch on 2/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudySessionApp/StudySessionApp/StudySessionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySessionAppApp.swift
+//  StudySessionApp
+//
+//  Created by Max Finch on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct StudySessionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
